0b23fd4dd9105575553f390b47b1f7ba7cc4e281
Felice Pollano <felice@felicepollano.com>
2017-11-03 15:38:42 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+"option strict";
+const electron = require("electron");
+const url = require("url");
+const path = require("path");
+
+const {app,BrowserWindow,Menu,dialog} = electron;
+
+let mainWindow;
+
+app.on("ready",()=>{
+    mainWindow = new BrowserWindow();
+    mainWindow.loadURL(url.format(
+        {
+            pathname:path.join(__dirname,"main.html"),
+            protocol:'file',
+            slashes: true
+        }
+    ));
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemlate);
+    Menu.setApplicationMenu(mainMenu);
+});
+
+var showOpen = function() {
+	dialog.showOpenDialog({ properties: [ 'openFile'], filters: [{ extensions: ['txt','log','*'] }]});
+};
+
+const mainMenuTemlate=[
+    {
+        label:"File",
+        submenu:[
+            {
+                label:"Add log",
+                click:()=>showOpen()
+            },
+            {
+                label:"Remove all"
+            },
+            {
+                label:"Exit",
+                accelerator:process.platform=="darwin"?'Command+Q':'Ctrl+Q',
+                click:()=>app.quit()
+            }
+        ]
+    }
+];
